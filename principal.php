1785e33565666cfa0918b112a58ee0d2104973ed
marlonrestrepo@hotmail.com <you@example.com>
2019-12-12 11:57:12 -0500

    rev 10
    primera revision

---
+++ principal.php
@@ -0,0 +1,15 @@
+<html>      <head>
+          <title>Suma</title>
+       </head>
+  <body>
+    <form method="post" action="pagina1.php">
+     Ingrese el valor del primer numero:
+     <input type="text" name="numero1">
+      <br>
+     Ingrese el valor del segundo numero:
+     <input type="text" name="numero2">
+      <br>
+    <input type="submit" value="confirmar">
+   </form>
+  </body>
+</html>
---
+++ suma.php
@@ -0,0 +1,8 @@
+<?php
+  $total;
+  $total = $_REQUEST['numero1']+$_REQUEST['numero2'];
+   echo "esto es una prueba";
+ 
+ echo "El total de la suma es: ".$total;
+    
+  ?>
